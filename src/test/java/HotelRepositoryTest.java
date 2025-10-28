011a6b621443b48e961c8ba65fa85be53000618b
Paul-Elian Tabarant <paul.tabarant@gmail.com>
2025-10-28 20:32:25 +0100

    refactor

--- src/test/java/HotelRepositoryTest.java
+++ src/test/java/HotelRepositoryTest.java
@@ -9,15 +9,15 @@ public class HotelRepositoryTest {
     @Test
     public void doitEnregistrerToutesLesChambresAvecLeurPrix() {
         HotelRepository repository = new InMemoryHotelRepository();
+
         repository.enregistrerHotel(Hotel.reconstruire(List.of(
                 new Chambre.Input(0, 1, 100.0),
                 new Chambre.Input(1, 10, 122.0),
                 new Chambre.Input(2, 20, 133.0)
         )));
-
         Hotel hotelRecupere = repository.recupererHotel();
-        List<Chambre.State> chambreStates = hotelRecupere.state().chambres();
 
+        List<Chambre.State> chambreStates = hotelRecupere.state().chambres();
         assertThat(chambreStates).hasSameElementsAs(List.of(
                 new Chambre.State(0, 1, 100.0),
                 new Chambre.State(1, 10, 122.0),
@@ -27,17 +27,14 @@ public class HotelRepositoryTest {
 
     @Test
     public void neDoitPasModifierLesChambresTantQuellesNeSontPasEnregistrees() {
-        HotelRepository repository = new InMemoryHotelRepository();
-
         Hotel hotel = Hotel.reconstruire(List.of(
                 new Chambre.Input(0, 1, 100.0),
                 new Chambre.Input(1, 10, 122.0)
         ));
 
+        HotelRepository repository = new InMemoryHotelRepository();
         repository.enregistrerHotel(hotel);
-
         hotel.modifierPrixChambresRezDeChaussee(10.0);
-
         Hotel hotelRecupere = repository.recupererHotel();
 
         assertThat(hotelRecupere.state().chambres()).hasSameElementsAs(List.of(
